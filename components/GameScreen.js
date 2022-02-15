1532e3cb408273fcc30512368bd8f2bb14ef9648
Adittya Dey <ichbinadittyadey@gmail.com>
2022-02-15 23:23:19 +0530

    added flip package

---
+++ components/GameScreen.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+const GameScreen = () => {
+    return (
+        <ScrollView style={styles.container}>
+
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+export default GameScreen;
